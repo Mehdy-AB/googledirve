ff126af5382f9757c5757883b96bb068d068b776
mehdi-pc-portable <mehdi.aoune25@gmail.com>
2024-12-14 10:53:03 +0000

    f

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,29 @@
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
 export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: ['react', 'jsx-a11y'], // Plugins should be strings
-    extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended'], // Use recommended configurations
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      jsxA11y,
+    },
     rules: {
-      'react/no-unknown-property': 'warn', // Enforces camelCase for DOM properties
-      'jsx-a11y/no-redundant-roles': 'warn', // Accessibility improvements
+      'react/no-unknown-property': 'warn',
+      'jsx-a11y/no-redundant-roles': 'warn',
     },
     settings: {
       react: {
-        version: 'detect', // Automatically detect the React version
+        version: 'detect',
       },
     },
   },
